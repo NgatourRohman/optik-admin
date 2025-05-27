440ed155d483b81ce95fb1c083093147614ace95
Ngatour Rohman <ngatourrohman25@gmail.com>
2025-05-27 22:25:16 +0700

    Add Product model, controller, migration, route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::resource('products', ProductController::class);
 });
