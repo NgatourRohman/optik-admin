d9d727470f22429db25cb050fc0ed740140e9740
Ngatour Rohman <ngatourrohman25@gmail.com>
2025-05-27 23:02:50 +0700

    Add order & order_item models, migrations, controller, dan routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\OrderController;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('orders', OrderController::class);
 });
